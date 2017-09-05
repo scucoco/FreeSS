0b37302cb12282d99444595390ceb07b855f1494
king <chineselittleboy@gmail.com>
2017-09-05 11:53:35 +0800

    更换地址，优化代码

--- index.js
+++ index.js
@@ -1,59 +1,95 @@
-var superagent = require('superagent');
-var cheerio = require('cheerio');
+const superagent = require('superagent');
+const cheerio = require('cheerio');
+const async = require('async');
 var nodemailer = require('nodemailer');
+const conf = require('./conf');
 
-var targetUrl = 'http://ss8.6gg6.net/#free'; //地址可能会换
-var hostRe = /http[s]?:\/\/\w+.\w+.\w+\//;
-var host = hostRe.exec(targetUrl);
-host = host[0];
+let url = conf.base_url;
+let qrdecodeParseUrl = conf.qrdecodParseeUrl;
 
-var smtpTransport = nodemailer.createTransport({
-    host: "smtp.qq.com",
-    secureConnection: true,
-    port: 465,
-    auth: {
-        user: "邮箱账号",
-        pass: "邮箱密码"
+superagent.get(url).end(function(err, res) {
+    if (err) {
+        return console.error(err);
     }
-});
 
-var mailOptions = {
-    from: "SSman <发送的邮箱账号>",
-    to: "接收的邮箱账号1, 接收的邮箱账号2",
-    subject: "最新账号",
-    html: ""
-}
-
-var imgSrc = [];
-
-superagent.get(targetUrl)
-    .end(function(err, res) {
+    let $ = cheerio.load(res.text);
+    let _redirect_url = $('#portfolio > div.container > div.row.text-center.center > a').attr('href');
+    superagent.get(_redirect_url).end(function(err, res) {
         if (err) {
             return console.error(err);
         }
-        var $ = cheerio.load(res.text);
-        var re = /http[s]?:\/\/.+/;
 
-        $('.container .icon-grid section img').each(function(idx, element) {
-            var $element = $(element);
-            var src = $element.attr("src");
-            if (!re.test(src)) {
-                _imgUrl = host + src;
-                imgSrc.push('<img src="' + _imgUrl + '" />');
-            }
-        });
+        let _redirects = res.redirects;
+        let _domain = _redirects[0]; //这里才是包含ss账号的网址
+        if (_domain != undefined) {
+            var imgs = [];
+            imgs.push(_domain + '/images/server01.png');
+            imgs.push(_domain + '/images/server02.png');
+            imgs.push(_domain + '/images/server03.png');
 
-        var html = imgSrc.join("<br/>");
-        mailOptions.html = html;
-        smtpTransport.sendMail(mailOptions, function(error, response) {
-            if (error) {
-                console.log(error);
-            } else {
-                console.log("Message sent: " + JSON.stringify(response));
-            }
-            smtpTransport.close();
-        });
+            //这里是把二维码解析成字符串
+            async.mapLimit(imgs, 5, function(obj, callback) {
+                superagent.get(qrdecodeParseUrl + obj).end(function(err, res) {
+                    if (err) {
+                        return console.error(err);
+                    }
+                    let _obj = {
+                        src: obj,
+                        ssTxt: res.text,
+                    };
+                    callback(null, _obj);
+                });
+
+            }, function(err, result) {
+                makeMessage(result);
+            });
+        }
+    });
+});
+
+function makeMessage(data) {
+    let html = conf.mail.template;
+    for (let i = 0; i < data.length; i++) {
+        let item = data[i];
+        if (item.ssTxt != 'error') {
+            let str = "";
+            str += "<tr>";
+            str += "<td><img src='" + item.src + "'/></td>";
+            str += "<td>" + item.ssTxt + "</td>";
+            str += "</tr>";
+            html += str;
+        }
+    }
+    html += "</table></body></html>";
+    sendMail(html);
+}
+
+function sendMail(message) {
+    let mail_conf = conf.mail;
+    let smtpTransport = nodemailer.createTransport({
+        host: mail_conf.host,
+        secureConnection: true,
+        port: mail_conf.port,
+        auth: {
+            user: mail_conf.auth.user,
+            pass: mail_conf.auth.pass
+        }
     });
 
-//定时任务(每4个小时的第一分钟获取一下)
-//1 */4 * * * node app.js
+    let mailOptions = {
+        from: mail_conf.mailOptions.from,
+        to: mail_conf.mailOptions.to,
+        subject: mail_conf.mailOptions.subject,
+        html: ""
+    }
+
+    mailOptions.html = message;
+    smtpTransport.sendMail(mailOptions, function(err, response) {
+        if (err) {
+            return console.error(err);
+        }
+
+        console.log("Message sent: " + JSON.stringify(response));
+        smtpTransport.close();
+    });
+}
